da279f6af787e60b808f326ebb08fabf14131374
David Ciria <thedacima99@gmail.com>
2020-05-06 20:36:52 +0200

    Added user interface

--- Assets/Scripts/wristPickUp.cs
+++ Assets/Scripts/wristPickUp.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class wristPickUp : MonoBehaviour
 {
@@ -8,6 +9,7 @@ public class wristPickUp : MonoBehaviour
     public Material activePickUpMaterial;
     public Material pickUpMaterial;
     public GameObject pickUps;
+    public GameObject UI;
 
     // Start is called before the first frame update
     void Start()
@@ -27,6 +29,7 @@ public class wristPickUp : MonoBehaviour
         {
             if (other.gameObject.GetComponent<PickUpScript>().isActivated)
             {
+                UI.GetComponent<UIScript>().elevateCounter();
                 other.gameObject.GetComponent<MeshRenderer>().material = pickUpMaterial;
                 other.gameObject.GetComponent<PickUpScript>().isActivated = false;
                 pickUps.GetComponent<PickUpColorChange>().StopCoroutine("changeColor");
